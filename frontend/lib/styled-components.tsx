33d9fa249557f1065cfa2227404e0e621125373f
ssumthingood <ssumthingood@gmail.com>
2023-09-05 17:37:57 +0900

    Fix : styled-component render error
    Former-commit-id: e2f4bba9b2de26954d238ed501741ce1d3eaaca3

--- frontend/lib/styled-components.tsx
+++ frontend/lib/styled-components.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { ServerStyleSheet, StyleSheetManager } from "styled-components";
-import {PropsWithChildren} from 'react';
+// import React from "react";
+// import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+// import {PropsWithChildren} from 'react';
 
-function useStyledComponentsRegistry() {
-  const [styledComponentsStyleSheet] = React.useState(
-    () => new ServerStyleSheet()
-  );
+// function useStyledComponentsRegistry() {
+//   const [styledComponentsStyleSheet] = React.useState(
+//     () => new ServerStyleSheet()
+//   );
 
-  const styledComponentsFlushEffect = () => {
-    const styles = styledComponentsStyleSheet.getStyleElement();
-    styledComponentsStyleSheet.seal();
-    return <>{styles}</>;
-  };
+//   const styledComponentsFlushEffect = () => {
+//     const styles = styledComponentsStyleSheet.getStyleElement();
+//     styledComponentsStyleSheet.seal();
+//     return <>{styles}</>;
+//   };
 
-  const StyledComponentsRegistry = ({ children}: PropsWithChildren) => (
-    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      {children as React.ReactElement}
-    </StyleSheetManager>
-  );
+//   const StyledComponentsRegistry = ({ children}: PropsWithChildren) => (
+//     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+//       {children as React.ReactElement}
+//     </StyleSheetManager>
+//   );
 
-  return [StyledComponentsRegistry, styledComponentsFlushEffect] as const;
-}
-export default useStyledComponentsRegistry;
+//   return [StyledComponentsRegistry, styledComponentsFlushEffect] as const;
+// }
+// export default useStyledComponentsRegistry;
