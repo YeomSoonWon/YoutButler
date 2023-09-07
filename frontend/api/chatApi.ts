71a30d4f5e0d5d8b654972f72f0752264813a0dc
ssumthingood <ssumthingood@gmail.com>
2023-09-07 17:57:12 +0900

    Refactor : API naming
    Former-commit-id: cfb74712040509441d9a22bc35a3f09b312e94b6

--- frontend/api/chatApi.ts
+++ frontend/api/chatApi.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const chatApi = axios.create({
+// 채팅 기능은 사용자 로그인 상태에서만 이용 가능
+const privateChatApi = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/chat`,
     headers:{
         "Content-Type" : "application/json",
     }
 })
+
+const chatApi = {
+
 
 export default chatApi;
