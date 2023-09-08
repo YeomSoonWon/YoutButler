5d32858b5d0f3f505d8eacc1e420657767d155bf
ssumthingood <ssumthingood@gmail.com>
2023-09-08 16:53:06 +0900

    Feat : Api template & Types definition for interface
    Former-commit-id: e9ab23d51da79a2e685511054395b29f54ff75bd

--- frontend/api/loanApi.ts
+++ frontend/api/loanApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 비로그인 시 가능한 로직
 const publicLoanApi = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/loan`,
+    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/loans`,
     headers:{
         "Content-Type" : "application/json",
     }
@@ -10,7 +10,7 @@ const publicLoanApi = axios.create({
 
 // 로그인 시 가능한 로직
 const privateLoanApi = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/loan`,
+    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/loans`,
     headers:{
         "Content-Type" : "application/json",
     }
