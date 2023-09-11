43f934b18a829a2029be7e5cf50fb745488dba75
boyoun1113 <boyoun1113@naver.com>
2023-09-11 15:42:20 +0900

    [S09P22A405-84] Feat: ChatTitleEach active기능 추가, Search 페이지 월세 전세 매매 클릭 시 input placeholder 변경
    Former-commit-id: 439213f2d2ee8c064d93ef420cb6af6e36513fef

--- frontend/components/List/ChatTitleEach.tsx
+++ frontend/components/List/ChatTitleEach.tsx
@@ -2,20 +2,21 @@
 
 import styled from "styled-components";
 import colors from "@/constants/colors";
+import { useState } from "react";
 
-const ChatTitleDiv = ({ title, color, weight }) => {
+const ChatTitleDiv = ({ title, isSelected, onClick }) => {
   const TitleNameDiv = styled.div`
-    background-color: ${color};
+    background-color: ${isSelected ? "#E8F9E9" : "white"};
     align-items: center;
     text-align: center;
     padding: 1.7rem 1rem;
-    font-weight: ${weight};
+    font-weight: ${isSelected ? "600" : null};
     color: ${colors.darkgray};
     border-bottom: solid 1px ${colors.lightgray};
   `;
 
   return (
-    <TitleNameDiv>
+    <TitleNameDiv onClick={onClick}>
       <TitleP>{title}</TitleP>
     </TitleNameDiv>
   );
