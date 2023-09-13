f9d4c1ba5288f75f010516ef0c8d7da64842626f
ssumthingood <ssumthingood@gmail.com>
2023-09-13 13:33:10 +0900

    Chore : add react-query-devtool & login logic info
    Former-commit-id: 56b6977bbda47a07a78f5edf42429630dd4c43b9

--- frontend/react-query/index.tsx
+++ frontend/react-query/index.tsx
@@ -1,17 +1,16 @@
 'use client'
 
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {PropsWithChildren} from 'react'
-import {  ReactNode } from 'react';
-
-// type Props = {
-//     children: ReactNode
-// }
 
 const queryClient = new QueryClient();
 
 const ReactQuery = ({ children }: PropsWithChildren) => {
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    return <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools />
+        </QueryClientProvider>
 }
 
 export default ReactQuery;
