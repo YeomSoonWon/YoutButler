0a6f78da094f0f5287cf340bb240488464eb778f
baiksungil <sibaek31@gmail.com>
2023-09-26 09:08:51 +0900

    Feat : 채팅 입력 연관 서비스 구현

--- backend/src/main/java/com/ficrew/yourbutler/Chat/application/facade/ChatFacade.java
+++ backend/src/main/java/com/ficrew/yourbutler/Chat/application/facade/ChatFacade.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatFacade {
 
-    private final CreateMessageProcessor createMessageProcessor;
+//    private final CreateMessageProcessor createMessageProcessor;
 
 
 
