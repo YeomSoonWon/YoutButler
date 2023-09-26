cfc2107a9c05bb7b05b5c81d1c4c9f0b8739631d
boyoun1113 <boyoun1113@naver.com>
2023-09-26 10:06:07 +0900

    [S09P22A405-170] Fix: 빌드 에러 수정
    Former-commit-id: 6be1e97e7bd03b695dba51f364734d8c82d64048

--- frontend/components/AppBar.tsx
+++ frontend/components/AppBar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { signIn, signOut, } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { Noto_Sans_KR, Sunflower } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import favicon from "@/app/favicon.ico";
 
 const notoSansKr = Noto_Sans_KR({
@@ -12,28 +12,32 @@ const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
 });
 
-const sunFlower = Sunflower({
-  weight: ["300", "500", "700"],
-  subsets: ["latin"],
-});
-
 const AppBar = ({ backgroundColor, color, user, logoLogout = false }) => {
   return (
     <ContainerDiv backgroundColor={backgroundColor}>
-      {
-      logoLogout ? 
-      <LogoutLogo color={color} onClick={()=>signOut()}>당신의집사</LogoutLogo>
-      :<Logo href={"/"} color={color}>당신의집사</Logo>
-      }
+      {logoLogout ? (
+        <LogoutLogo color={color} onClick={() => signOut()}>
+          당신의집사
+        </LogoutLogo>
+      ) : (
+        <Logo href={"/"} color={color}>
+          당신의집사
+        </Logo>
+      )}
       <NavDiv className={notoSansKr.className}>
-        <StyledLink href="/search" color={color}>매물검색</StyledLink>
+        <StyledLink href="/search" color={color}>
+          매물검색
+        </StyledLink>
         {/* <StyledLink href="" color={color}>알림</StyledLink> */}
         {user ? (
           <>
-          <StyledLink href="/mypage" color={color}>마이페이지</StyledLink>
+            <StyledLink href="/mypage" color={color}>
+              마이페이지
+            </StyledLink>
             <img
-            src={user?.picture ? user?.picture: favicon.src}
-            style={{width:"30px", height:"30px", backgroundColor:"white"}}/>
+              src={user?.picture ? user?.picture : favicon.src}
+              style={{ width: "30px", height: "30px", backgroundColor: "white" }}
+            />
             <StyledUserName color={color}>{user?.nickname}</StyledUserName>
             <YellowBtn onClick={() => signOut()}>로그아웃</YellowBtn>
           </>
@@ -47,43 +51,43 @@ const AppBar = ({ backgroundColor, color, user, logoLogout = false }) => {
   );
 };
 
-interface BgProp{
-  backgroundColor?:string;
+interface BgProp {
+  backgroundColor?: string;
 }
 
-interface ColorProp{
-  color?:string;
+interface ColorProp {
+  color?: string;
 }
 
 const ContainerDiv = styled.div<BgProp>`
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 1.5rem 4rem;
-background-color: ${(props)=>props.backgroundColor || "#334835"};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.5rem 4rem;
+  background-color: ${(props) => props.backgroundColor || "#334835"};
 `;
 
 const Logo = styled(Link)<ColorProp>`
-font-size: 1.5rem;
-color: ${(props)=>props.color || "white"};
-text-decoration-line: none;
+  font-size: 1.5rem;
+  color: ${(props) => props.color || "white"};
+  text-decoration-line: none;
 `;
 
 const LogoutLogo = styled.div<ColorProp>`
-font-size: 1.5rem;
-color: ${(props)=>props.color || "white"};
-text-decoration-line: none;
+  font-size: 1.5rem;
+  color: ${(props) => props.color || "white"};
+  text-decoration-line: none;
 `;
 /* ${({ theme }) => theme.logoFont || ""}; */
 
 const StyledLink = styled(Link)<ColorProp>`
-color: ${(props)=>props.color || "white"};
-text-decoration-line: none;
+  color: ${(props) => props.color || "white"};
+  text-decoration-line: none;
 `;
 
 const StyledUserName = styled.p<ColorProp>`
-color: ${(props)=>props.color || "white"};
-text-decoration-line: none;
+  color: ${(props) => props.color || "white"};
+  text-decoration-line: none;
 `;
 
 const YellowBtn = styled.button`
@@ -104,7 +108,7 @@ const NavDiv = styled.div`
   /* width: 24rem; */
   display: flex;
   justify-content: space-between;
-  gap : 3rem;
+  gap: 3rem;
   align-items: center;
 `;
 
