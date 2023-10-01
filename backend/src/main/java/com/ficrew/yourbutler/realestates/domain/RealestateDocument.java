02e40f32a7249f045dafc147f1ec319135ce5277
윤다정 <ydajeong7@gmail.com>
2023-10-02 01:16:35 +0900

    Feat: ES Springboot 기본설정

---
+++ backend/src/main/java/com/ficrew/yourbutler/realestates/domain/RealestateDocument.java
@@ -0,0 +1,61 @@
+package com.ficrew.yourbutler.realestates.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(indexName = "realestate")
+@Mapping(mappingPath = "elastic/realestate-mapping.json")
+@Setting(settingPath = "elastic/realestate-setting.json")
+public class RealestateDocument {
+
+    @Id
+    private Integer articleNo;
+    private Integer complexNo;
+    private Integer hscpNo;
+    private Integer ptpNo;
+    @Field(type = FieldType.Text, analyzer = "korean_mixed") // Nori 분석기 사용
+    private String complexName;
+    private String sidoName;
+    private String guName;
+    private String dongName;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private String roomType;
+    private String realEstateTypeName;
+    private String floorInfo;
+    private String buildingName;
+    private String dealOrWarrantPrc;
+    private Integer rentPrc;
+    private Double supplyArea;
+    private Double exclusiveArea;
+    private Double exclusiveRate;
+    private String direction;
+    private List<String> tagList; // TODO
+    private String description;
+    private String realtorName;
+    private String realtorAddress;
+    private String realtorcellPhoneNo;
+    private Date articleConfirmYmd; // TODO
+    private Integer roomCnt;
+    private Integer bathroomCnt;
+    private Integer maintenanceFee;
+    private String lawUsage;
+    private Date approvalDate; // TODO
+    private Object formatted_image_list; // TODO
+    private Integer years_difference;
+
+}
---
+++ backend/src/main/java/com/ficrew/yourbutler/realestates/infrastructure/config/ElasticSearchConfig.java
@@ -0,0 +1,19 @@
+package com.ficrew.yourbutler.realestates.infrastructure.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        // http port 와 통신할 주소
+        ClientConfiguration configuration = ClientConfiguration.builder().connectedTo("j9a405.p.ssafy.io:3200").build();
+        return RestClients.create(configuration).rest();
+    }
+}
