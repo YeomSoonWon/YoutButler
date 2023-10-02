bb896eb9606717e99bed5ffec1fd49670e8f3bd5
baiksungil <sibaek31@gmail.com>
2023-10-02 12:54:05 +0900

    Feat : chat 조회, 채팅 입력 API 구현

--- backend/src/main/java/com/ficrew/yourbutler/Chat/infrastructure/persistence/ChatRepositoryAdapter.java
+++ backend/src/main/java/com/ficrew/yourbutler/Chat/infrastructure/persistence/ChatRepositoryAdapter.java
@@ -3,10 +3,11 @@ package com.ficrew.yourbutler.Chat.infrastructure.persistence;
 import com.ficrew.yourbutler.Chat.domain.entity.ChatRoom;
 import com.ficrew.yourbutler.Chat.domain.repository.ChatRepository;
 import com.ficrew.yourbutler.Chat.infrastructure.persistence.jpa.JpaChatRoomRepository;
-import com.ficrew.yourbutler.Chat.infrastructure.persistence.jpa.JpaMessageRepository;
 import com.ficrew.yourbutler.member.domain.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,13 +21,13 @@ public class ChatRepositoryAdapter implements ChatRepository {
     }
 
     @Override
-    public ChatRoom findByMemberAndAptId(Member member, Long aptId) {
+    public Optional<ChatRoom> findByMemberAndAptId(Member member, Long aptId) {
         return chatRoomRepository.findByMemberAndAptId(member, aptId);
     }
 
     @Override
-    public ChatRoom findById(Long chatRoomId) {
-        return chatRoomRepository.findById(chatRoomId).get();
+    public Optional<ChatRoom> findById(Long chatRoomId) {
+        return chatRoomRepository.findById(chatRoomId);
     }
 
 }
