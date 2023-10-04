1f747d723ed6bc62fc3edd22a8b1c19914ea99cb
yumsw6109 <yumsw6109@gmail.com>
2023-10-05 02:56:48 +0900

    매물 상세 페이지 수정
    Former-commit-id: b272a0e1e626e4067b069ffa61677fe1e1437847

--- frontend/api/realEstateApi.ts
+++ frontend/api/realEstateApi.ts
@@ -1,21 +1,21 @@
 import { RoomSearchResult } from "@/types/Room";
 import axios, { Axios, AxiosRequestConfig } from "axios";
 
-interface RealEstateSearchTerm {
-    type: String,
-    room_type: String,
-    deposit_min?: Number,
-    deposit_max?: Number,
-    deal_price?: Number,
-    rent_price?: Number,
-    maintenance_fee?: Number,
-    room_size?: Number,
-    use_approve_y?: Number,
-    holding_asset?: Number,
-    additional_asset?: Number,
-    credit_rating?: Number,
-    monthly_availableAsset?: Number,
-};
+// interface RealEstateSearchTerm {
+//     type: String,
+//     room_type: String,
+//     deposit_min?: Number,
+//     deposit_max?: Number,
+//     deal_price?: Number,
+//     rent_price?: Number,
+//     maintenance_fee?: Number,
+//     room_size?: Number,
+//     use_approve_y?: Number,
+//     holding_asset?: Number,
+//     additional_asset?: Number,
+//     credit_rating?: Number,
+//     monthly_availableAsset?: Number,
+// };
 
 const publicEstateApi: Axios = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/realestates`,
