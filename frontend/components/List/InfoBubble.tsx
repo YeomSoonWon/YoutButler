46aee094e0a7bdea35602fc8771ab61c3602348c
boyoun1113 <boyoun1113@naver.com>
2023-10-06 03:28:58 +0900

    [S09P22A405-211]
    Former-commit-id: b04a0d27fe26de9c38b0030bbd6850cbc8005576

--- frontend/components/List/InfoBubble.tsx
+++ frontend/components/List/InfoBubble.tsx
@@ -1,6 +1,7 @@
 import React, { useState, PropsWithChildren, useEffect } from 'react';
 import styled from 'styled-components';
 import ColorDot from './ColorDot';
+import colors from '@/constants/colors';
 
 // interface Props {
 //   // visible?: Boolean;
@@ -35,13 +36,26 @@ const InfoBubble = ({
       setContent(
         <InnerBubble>
           <div>🏠 당신의 집사 월세 계산방법 안내 🏠</div>
-
+          <div>❗현재 자산이 보증금 보다 같거나 많은 경우❗</div>
+          <RowDiv><ColorDot color="yellowgreen" />구입 가능</RowDiv>
+          <div>❗현재 자산이 보증금 보다 적은 경우❗</div>
+          <div>1. (현재자산과 보증금 차이 x 신용도 기반 이자율 + 월세)보다 월 여유자금이 같거나 많은 경우</div>
+          <RowDiv><ColorDot color={colors.yellow} />조금 부족</RowDiv>
+          <div>2. (현재자산과 보증금 차이 x 신용도 기반 이자율 + 월세)보다 월 여유자금이 적은 경우</div>
+          <RowDiv><ColorDot color={colors.red} />불가능</RowDiv>
         </InnerBubble>
       );
     } else if (contentId === 'LEASE') {
       setContent(
         <InnerBubble>
           <div>🏠 당신의 집사 전세 계산방법 안내 🏠</div>
+          <div>❗현재 자산이 보증금 보다 같거나 많은 경우❗</div>
+          <RowDiv><ColorDot color="yellowgreen" />구입 가능</RowDiv>
+          <div>❗현재 자산이 보증금 보다 적은 경우❗</div>
+          <div>1. 현재 자산이 전세보증금의 30% 이상에 해당하는 경우</div>
+          <RowDiv><ColorDot color={colors.yellow} />조금 부족</RowDiv>
+          <div>2. 현재 자산이 전세보증금의 30% 미만에 해당하는 경우</div>
+          <RowDiv><ColorDot color={colors.red} />불가능</RowDiv>
         </InnerBubble>
       );
     } else if (contentId === 'DEAL') {
@@ -51,8 +65,10 @@ const InfoBubble = ({
           <div>❗현재 자산이 매매가 보다 같거나 많은 경우❗</div>
           <RowDiv><ColorDot color="yellowgreen" />구입 가능</RowDiv>
           <div>❗현재 자산이 매매가 보다 작은 경우❗</div>
-          <div>LTV 적용 가격(대출 최대 가능 금액) + 현재 자산이 매매가 보다 같거나 많은 경우: </div>
-          <RowDiv><ColorDot color="yellowgreen" />구입 가능</RowDiv>
+          <div>1. LTV 적용 가격(대출 최대 가능 금액) + 현재 자산이 매매가 보다 같거나 많은 경우 : </div>
+          <RowDiv><ColorDot color={colors.yellow} />조금 부족</RowDiv>
+          <div>2. LTV 적용 가격(대출 최대 가능 금액) + 현재 자산이 매매가 보다 적은 경우 : </div>
+          <RowDiv><ColorDot color={colors.red} />불가능</RowDiv>
         </InnerBubble>
       );
       // content = content.replace(/(?:\r\n|\r|\n)/g, "<br />");
@@ -82,7 +98,6 @@ const Bubble = styled.div<{ props: string }>`
   visibility: ${(props) => props.props};
   transition: opacity 0.3s;
   width: 20rem;
-  height: 19rem;
   text-align: center;
 `;
 
