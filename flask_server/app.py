9858552b029fedf763938780b384e3ad130c4a95
baiksungil <sibaek31@gmail.com>
2023-10-06 08:05:02 +0900

    Fix : 월 평균 상환금액 반환
    Former-commit-id: d4ebaa1f20ea4da4bd447f6bfc6a7d50515af974

--- flask_server/app.py
+++ flask_server/app.py
@@ -24,8 +24,7 @@ def chat():
         "loan_interest": 대출 상품의 정보가 주어진다면 float 형태의 대출 상품의 금리가 들어갑니다 대출 상품의 정보가 없다면 빈 값이 들어갑니다,
         "bank_name": 대출 상품의 정보가 주어진다면 대출 상품을 제공하는 은행의 이름이 들어갑니다 대출 상품의 정보가 없다면 빈 값이 들어갑니다,
         "message": 대출 상품의 정보가 주어진다면 대출 상품에 대한 설명이 들어갑니다 대출 상품의 정보가 없다면 빈 값이 들어갑니다,
-        "paying_off_period": 대출 상품의 정보가 주어지고 사용자의 채팅에 상환 기간에 대한 내용이 있었다면 그 값이 int 형태로 들어갑니다 그렇지 않다면 빈 값이 들어갑니다,
-        "paying_off_amount": 대출 상품의 정보가 주어지고 사용자의 채팅에 상환 금액에 대한 내용이 있었다면 그 값이 int 형태로 들어갑니다 그렇지 않다면 빈 값이 들어갑니다,
+        "paying_off_period": 대출 상품의 정보가 주어지고 사용자의 채팅에 대출의 상환 기간에 대한 내용이 있었다면 그 값이 int 형태로 들어갑니다 그렇지 않다면 빈 값이 들어갑니다,
         "information": 금융 용어에 대한 정보가 주어진다면 금융 용어에 대한 설명이 들어갑니다 금융 용어에 대한 정보가 없다면 빈 값이 들어갑니다.
     }}
     
@@ -36,10 +35,13 @@ def chat():
     금융 용어의 정보는 아래와 같습니다.
     
     {financial_information}
+    
+    사용자의 채팅 정보는 아래와 같습니다.
+    {user_message}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["database_information", "financial_information"],
+        input_variables=["database_information", "financial_information", "user_message"],
         template=summary_template
         # 필수 입력 변수의 하위 집합을 지정할 수 있음.
         # partial_variables={
@@ -54,7 +56,7 @@ def chat():
     data = json.loads(intention)
     loan_data = query_loan_chain(data.get('loan'))
     financial_data = get_response_from_query(data.get('finance'))
-    response = chain.run(database_information=loan_data, financial_information=financial_data)
+    response = chain.run(database_information=loan_data, financial_information=financial_data, user_message=user_message)
     print(response)
     data = json.loads(response)
     print(data)
